b72ef70f73f8c686026c7bd53c031887338b8d79
Serg3 <jaguar.sergey@gmail.com>
2018-04-15 14:08:29 +0300

    refactoring the response

--- app.rb
+++ app.rb
@@ -5,9 +5,15 @@ class App
   def call(env)
     @request = Rack::Request.new(env)
 
-    if @request.path_info == '/time'
-      time_output = TimeFormat.new(@request.params)
-      http_response(time_output.status, time_output.body)
+    case @request.path_info
+    when '/time'
+      response = TimeFormat.new(@request.params)
+
+      if response.unknown_formats.empty?
+        http_response(200, Time.now.strftime(response.time_format))
+      else
+        http_response(400, "Unknown time format #{response.unknown_formats}")
+      end
     else
       http_response(404, 'Not Found')
     end
--- time_format.rb
+++ time_format.rb
@@ -1,6 +1,6 @@
 class TimeFormat
 
-  attr_reader :status, :body
+  attr_reader :time_format, :unknown_formats
 
   FORMATS = {
     'year' => ' %Y -', 'month' => ' %m -', 'day' => ' %d ',
@@ -10,32 +10,12 @@ class TimeFormat
   def initialize(params)
     @time_format = ''
     @unknown_formats = []
-    @body = set_time_output(params)
+    set_time_output(params['format'].split(','))
   end
 
   private
 
   def set_time_output(params)
-    set_params(params['format'].split(','))
-
-    if @unknown_formats.empty?
-      time_output
-    else
-      bad_format
-    end
-  end
-
-  def bad_format
-    @status = 400
-    "Unknown time format #{@unknown_formats}"
-  end
-
-  def time_output
-    @status = 200
-    Time.now.strftime(@time_format)
-  end
-
-  def set_params(params)
     params.each do |format|
       if FORMATS[format]
         @time_format += FORMATS[format]
